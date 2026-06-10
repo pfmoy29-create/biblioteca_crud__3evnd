da6aea59575ce3a73ba32a65e6ec8b1ce30659f4
Jimmy Moises Perez Fernandez <pfmoy29@gmail.com>
2026-06-10 10:40:21 -0600

    Agregando modelos de autor y carrera

---
+++ Models/libro.py
@@ -0,0 +1,30 @@
+#clase libro
+class Libro:
+
+    #Metodo constructor
+    def __init__(self, id ,titulo, autor, isbn):
+        self. id = id
+        self. titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponoble = True
+
+    #Mostrar en la pantalla la informacion de un libro
+    def mostrar_info(self):
+        estado = " Disponible" if self.disponoble else "Prestado"
+        return f"{self.titulo} - {self.autor}: {estado}"
+
+    
+    def prestar(self):
+        if self.disponoble:
+            self.disponoble = False
+            print(f"El libro {self.titulo} esta prestado")
+            return True
+
+        else:
+            print(f"El libro{self.titulo} no esta disponible")
+            return False
+
+    def devolver(self):
+        self.disponoble = True
+        print(f"El libro{self.titulo} ha sido devuelto")
