4410bd2cb2da185840d367eba6a4a5e9d6caf827
Jimmy Moises Perez Fernandez <pfmoy29@gmail.com>
2026-06-15 12:38:57 -0600

    LibroDAO

---
+++ app.py
@@ -0,0 +1,21 @@
+from dao.libro_dao import LibroDAO
+from Models.libro import Libro
+
+def main():
+    try:
+        Libro_dao = LibroDAO()
+        libros = Libro_dao.obtener_libros()
+
+        print("Libros en la biblioteca:")
+        if len(libros) == 0:
+           print("No hay libros resgistrados")
+        else :
+            for libro in libros:
+                print(f"{libro.titulo} -{libro.autor} - {libro.disponible}")
+        print("\n Conexion exitosa a la base de datos")
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+
+if __name__ == "__main__":
+    main()
+    
