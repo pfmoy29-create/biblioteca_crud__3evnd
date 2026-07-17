9d106d5877b4a2fe3aba7c8fcde7a8526bc7c313
Jimmy Moises Perez Fernandez <pfmoy29@gmail.com>
2026-07-17 10:00:15 -0600

    Insertar grafico a app

--- ui/libro_form.py
+++ ui/libro_form.py
@@ -1,5 +1,8 @@
 import flet as ft 
 
+from Models.libro import Libro
+from dao.libro_dao import LibroDAO
+
 def libro_form(regresar):
     titulo_input = ft.TextField(
         label = "Título del libro: ",
@@ -31,13 +34,38 @@ def libro_form(regresar):
         if titulo == "" or autor == "" or isbn == "":
             mensaje.value = "Todos los campos obligatorios"
             mensaje.color = ft.Colors.RED
-        else:
+            e.page.update()
+            return
+        try:
+            libro_dao = LibroDAO()
+            id = libro_dao.obtener_ultimo_id() + 1
+
+            nuevo_libro = Libro(
+                id = id,
+                titulo= titulo,
+                autor= int(autor),
+                isbn= isbn,
+                disponible= True
+            )
+
+            libro_dao.insertar(nuevo_libro)
+
+
+
             mensaje.value = f"Libro '{titulo}' guardado correctamente"
             mensaje.color = ft.Colors.GREEN
-            print(f"Titulo: { titulo}, autor: {autor}, isbn: {isbn}")
+            #Limpiar los campos de entrada
             titulo_input.value = ""
             autor_input.value = ""
             isbn_input.value = ""
+
+        except ValueError:
+            mensaje.value = "El campo 'Autor' debe ser un numero entero"
+            mensaje.color = ft.Colors.RED
+
+        except Exception as ex:
+            mensaje.value = f"Error al guargar el libro: {ex}"
+            mensaje.color = ft.Colors.RED
 
         e.page.update()
 
